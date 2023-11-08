43f7c8fe4ec3a9c713d9e138a085aff8d439c40a
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-11-08 20:13:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/esjumbo/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.esjumbo.data
+
+import com.example.esjumbo.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanila,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lychee,
+    )
+}
