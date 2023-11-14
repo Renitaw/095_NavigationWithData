35a3c86939907defd2e401f24040a59113380bb3
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-11-14 09:41:32 +0700

    Membuat HalamanForm.kt

---
+++ app/src/main/java/com/example/esjumbo/HalamanForm.kt
@@ -0,0 +1,66 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.esjumbo.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanForm(
+    onSubmitButtonClick: (MutableList<String>) -> Unit
+) {
+    var namaTxt by remember {
+        mutableStateOf("")
+    }
+    var alamatTxt by remember {
+        mutableStateOf("")
+    }
+    var telponTxt by remember {
+        mutableStateOf("")
+    }
+
+    var lisDataTxt: MutableList<String> = mutableListOf(namaTxt, alamatTxt, telponTxt)
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        OutlinedTextField(
+            value = namaTxt,
+            onValueChange = {namaTxt = it},
+            label = { Text(text = "Nama") })
+
+        OutlinedTextField(
+            value = alamatTxt,
+            onValueChange = {alamatTxt = it},
+            label = { Text(text = "Alamat") })
+
+        OutlinedTextField(
+            value = telponTxt,
+            onValueChange = {telponTxt = it},
+            label = { Text(text = "Telepon") })
+
+        Spacer(modifier = Modifier.padding(16.dp))
+
+        Button(onClick =  {onSubmitButtonClick(lisDataTxt)} ) {
+            Text(text = stringResource(R.string.next))
+        }
+     }
+}
